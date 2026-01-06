1d3f99f2cdad378e79d8ad4fa756e8b872f8b68a
dasrohit15 <dasrohit1636@gmail.com>
2026-01-06 23:19:09 +0530

    cchnages

--- backend/main.py
+++ backend/main.py
@@ -31,3 +31,7 @@ async def ask(query: Query):
 
 #if __name__ == "__main__":
     #uvicorn.run("main:app", host="127.0.0.1", #port=8000, reload=True)
+
+@app.get("/")
+def root():
+    return {"message": "SafeSpace backend is running!"}
